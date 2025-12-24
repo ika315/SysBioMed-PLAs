0a24f1c989f78aed1169eb5816d2cbcc47337c3b
Irem Kaciran <irem@i082.tum.vpn.lrz.de>
2025-12-24 10:25:38 +0100

    higher time limit

--- run_benchmarking_platelets.sh
+++ run_benchmarking_platelets.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=100000
-#SBATCH --time=15:00:00
+#SBATCH --time=30:00:00
 #SBATCH --output=/nfs/home/students/i.kaciran/SysBioMed-PLAs/slurm_logs/vision_%j.out
 #SBATCH --error=/nfs/home/students/i.kaciran/SysBioMed-PLAs/slurm_logs/vision_%j.err
 
