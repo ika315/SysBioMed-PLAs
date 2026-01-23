65d446ad27d23ec6f134016a3f3bc8e0d9784735
Irem Kaciran <irem@g099.tum.vpn.lrz.de>
2026-01-23 22:30:52 +0100

    shell scritp

---
+++ run_thresholding.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=thresholding
+#SBATCH --partition=compms-cpu-big
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64000
+#SBATCH --time=15:00:00
+#SBATCH --output=/nfs/home/students/i.kaciran/SysBioMed-PLAs/slurm_logs/thresholding_%j.out
+#SBATCH --error=/nfs/home/students/i.kaciran/SysBioMed-PLAs/slurm_logs/thresholding_%j.err
+
+source /opt/mambaforge/etc/profile.d/conda.sh
+conda activate pla
+
+echo "Running on node: $(hostname)"
+echo "CPUs: $SLURM_CPUS_PER_TASK"
+echo "Memory: $SLURM_MEM_PER_NODE MB"
+
+Rscript src/thresholding/thresholding_variants.R
